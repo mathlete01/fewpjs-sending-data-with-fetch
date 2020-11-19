a5eb3679917459a958225159d9a74507fd57e5fc
mathlete01 <msallin@gmail.com>
2020-11-19 09:37:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// Test 1 - Send Data
+// In submitData, write a valid POST request to http://localhost:3000/users using fetch(). This request should include:
+
+// The destination URL
+// Headers for 'Content-Type' and 'Accept' set to 'application/json'
+// A body with the name and email passed in as arguments to submitData. These should be assigned to name and email keys within an object. This object should then be stringified.
+
+function submitData(name, email){
+
+    const output = document.querySelector("body")
+
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+      .then(function(object) {
+        output.innerHTML = object.id
+
+        console.log(object.id);
+    })
+    .catch(function(error) {
+        output.innerHTML = error.message
+        });    
+}
+
+//submitData("foo", "foo@foo.com")
+//submitData()
